d1bb97b3d4f511ae579e13d4863eb9f97b859f89
valen-tino <59046897+valen-tino@users.noreply.github.com>
2022-02-23 10:45:54 +0800

    Update Material : Variables

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 # ini hanya komentar
-print("Hallo!")
-print("2+5",2+5)
+def main():
+    celcius = eval(input("Masukkan nilai celcius : "))
+    fahrenheit = (9/5) * celcius + 32
+    print ("Fahrenheit : " + str(fahrenheit))
+
+    fahrenheit = eval(input("Masukkan nilai fahrenheit : "))
+    celcius = (fahrenheit - 32) * (5/9)
+    print ("Celcius : " + str(celcius))
+main()
